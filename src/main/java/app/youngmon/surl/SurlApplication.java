d02648ea08f041db26a95573c4e80b3265e60c70
youngpar <youngseo321@gmail.com>
2023-09-24 20:50:26 +0900

    Initialzated App

---
+++ src/main/java/app/youngmon/surl/SurlApplication.java
@@ -0,0 +1,11 @@
+package app.youngmon.surl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurlApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SurlApplication.class, args);
+	}
+}
---
+++ src/test/java/app/youngmon/surl/SurlApplicationTests.java
@@ -0,0 +1,13 @@
+package app.youngmon.surl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
