8b9129f41647aa9bf7b99dd7f2d19e880a2d5276
youngpar <youngseo321@gmail.com>
2023-09-25 16:27:22 +0900

    Implemented Hash Cache Test

--- src/test/java/app/youngmon/surl/unit/hash/HashCacheTest.java
+++ src/test/java/app/youngmon/surl/unit/hash/HashCacheTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Hash Cache Test")
@@ -14,20 +17,31 @@ public class HashCacheTest {
     HashCache mockHashCache;
 
     @Test
+    @DisplayName("createKV Test")
     void    createKVTest() {
         //  given
+        String  key = "key";
+        String  value = "value";
+        when(mockHashCache.createKV(key, value)).thenReturn(value);
 
         //  when
+        String  res = mockHashCache.createKV(key, value);
 
         //  then
+        assertThat(res).isEqualTo(value);
     }
 
     @Test
     void    findUrlByKeyTest() {
         //  given
+        String  key = "key";
+        String  expectedUrl = "url";
+        when(mockHashCache.findUrlByKey(key)).thenReturn(expectedUrl);
 
         //  when
+        String  res = mockHashCache.findUrlByKey(key);
 
         //  then
+        assertThat(res).isEqualTo(expectedUrl);
     }
 }
