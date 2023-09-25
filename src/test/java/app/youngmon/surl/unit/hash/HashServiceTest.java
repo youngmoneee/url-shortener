c70cf305dd5f8c3cf90949b4e96579bca41fd466
youngpar <youngseo321@gmail.com>
2023-09-25 16:46:29 +0900

    Created HashServiceTest File

---
+++ src/test/java/app/youngmon/surl/unit/hash/HashServiceTest.java
@@ -0,0 +1,59 @@
+package app.youngmon.surl.unit.hash;
+
+import app.youngmon.surl.HashService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("HashService Test")
+public class HashServiceTest {
+    @Mock
+    HashService mockHashService;
+
+    @Test
+    @DisplayName("getHashBase Test")
+    void getHashBaseTest() {
+        //  given
+
+        //  when
+
+        //  then
+
+    }
+
+    @Test
+    @DisplayName("getHashBaseLength Test")
+    void getHashBaseLengthTest() {
+        //  given
+
+        //  when
+
+        //  then
+
+    }
+
+    @Test
+    @DisplayName("encode Test")
+    void encodeTest() {
+        //  given
+
+        //  when
+
+        //  then
+
+    }
+
+    @Test
+    @DisplayName("decode Test")
+    void decodeTest() {
+        //  given
+
+        //  when
+
+        //  then
+
+    }
+}
