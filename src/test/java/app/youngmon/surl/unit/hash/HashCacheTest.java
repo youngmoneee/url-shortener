e2918278b7bbb748ae890583d36c61600912735e
youngpar <youngseo321@gmail.com>
2023-09-25 16:15:28 +0900

    Created Hash Cache Unit Test File

---
+++ src/test/java/app/youngmon/surl/unit/hash/HashCacheTest.java
@@ -0,0 +1,33 @@
+package app.youngmon.surl.unit.hash;
+
+import app.youngmon.surl.HashCache;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Hash Cache Test")
+public class HashCacheTest {
+    @Mock
+    HashCache mockHashCache;
+
+    @Test
+    void    createKVTest() {
+        //  given
+
+        //  when
+
+        //  then
+    }
+
+    @Test
+    void    findUrlByKeyTest() {
+        //  given
+
+        //  when
+
+        //  then
+    }
+}
