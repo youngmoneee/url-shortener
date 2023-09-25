41359dd7776a35569c2ba93d6b2d815dbf8fea32
youngpar <youngseo321@gmail.com>
2023-09-25 16:32:14 +0900

    Created Hash Repository Unit Test File

---
+++ src/test/java/app/youngmon/surl/unit/hash/HashRepositoryTest.java
@@ -0,0 +1,35 @@
+package app.youngmon.surl.unit.hash;
+
+import app.youngmon.surl.HashRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("HashRepository Test")
+public class HashRepositoryTest {
+    @Mock
+    HashRepository mockHashRepository;
+
+    @Test
+    @DisplayName("createUrl Test")
+    void createUrlTest() {
+        //  given
+
+        //  when
+
+        //  then
+    }
+
+    @Test
+    @DisplayName("findUrlById Test")
+    void findUrlByIdTest() {
+        //  given
+
+        //  when
+
+        //  then
+    }
+}
