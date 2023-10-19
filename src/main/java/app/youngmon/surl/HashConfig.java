88edd9ff7c63f3d187bb385bc025372208ca624c
youngpar <youngseo321@gmail.com>
2023-10-19 22:51:11 +0900

    Const Value -> conf

--- src/main/java/app/youngmon/surl/HashConfig.java
+++ src/main/java/app/youngmon/surl/HashConfig.java
@@ -3,15 +3,11 @@ package app.youngmon.surl;
 import app.youngmon.surl.interfaces.HashCache;
 import app.youngmon.surl.interfaces.HashJpaRepository;
 import app.youngmon.surl.interfaces.HashService;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HashConfig {
-    @Value("${hash.base}")
-    String hashBase;
-
     @Bean
     public HashCache hashCache() {
         return new HashCacheImpl();
@@ -19,6 +15,6 @@ public class HashConfig {
 
     @Bean
     public HashService hashService(HashJpaRepository hashRepository) {
-        return new HashServiceImpl(hashBase, hashCache(), hashRepository);
+        return new HashServiceImpl(hashCache(), hashRepository);
     }
 }
